77732c647c28591820c352864044c06430d5e35e
AleoIOU <133340871+AleoIOU@users.noreply.github.com>
2023-05-15 18:41:47 +0800

    creditor rename to lender, debtor rename to borrower

--- run.sh
+++ run.sh
@@ -6,25 +6,25 @@ then
     exit
 fi
 
-# The private key and address of the debtor.
-# Swap these into program.json, when running transactions as the debtor.
+# The private key and address of the borrower.
+# Swap these into program.json, when running transactions as the borrower.
 # "private_key": "APrivateKey1zkpBg8aN5h7rsSUHNEGZetFpUn7dgoutZhorCS1veaiH4YZ"
 # "address": aleo1z5eut5f5x7r45c8ut3xmt5wrsxdkpjrtksn5pu3d9af3zsr8ju8sm64tkz
 
-# The private key and address of the creditor.
-# Swap these into program.json, when running transactions as the creditor.
+# The private key and address of the lender.
+# Swap these into program.json, when running transactions as the lender.
 # "private_key": "APrivateKey1zkpG9Af9z5Ha4ejVyMCqVFXRKknSm8L1ELEwcc4htk9YhVK"
 # "address": aleo1yzlta2q5h8t0fqe0v6dyh9mtv4aggd53fgzr068jvplqhvqsnvzq7pj2ke
 
 echo "
 ###############################################################################
 ########                                                               ########
-########             Debtor applies for loan from creditor.            ########
+########             Borrower applies for loan from lender.            ########
 ########                                                               ########
 ###############################################################################
 "
 
-# Swap in the private key and address of the debtor to program.json.
+# Swap in the private key and address of the borrower to program.json.
 echo "{
   \"program\": \"iou.aleo\",
   \"version\": \"0.0.0\",
@@ -44,7 +44,7 @@ echo "{
 echo "
 ###############################################################################
 ########                                                               ########
-########               Creditor disburses loan to debtor.              ########
+########               Lender disburses loan to borrower.              ########
 ########                                                               ########
 ###############################################################################
 "
@@ -68,7 +68,7 @@ echo "{
 echo "
 ###############################################################################
 ########                                                               ########
-########                Debtor repays loan to creditor.                ########
+########                Borrower repays loan to lender.                ########
 ########                                                               ########
 ###############################################################################
 "
@@ -92,7 +92,7 @@ echo "{
 echo "
 ###############################################################################
 ########                                                               ########
-########   The creditor confirms the repayment and settles the loan.   ########
+########    The lender confirms the repayment and settles the loan.    ########
 ########                                                               ########
 ###############################################################################
 "
