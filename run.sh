852718432b221a395d849716567ca02855835221
AleoIOU <133340871+AleoIOU@users.noreply.github.com>
2023-05-15 16:21:36 +0800

    code comment

--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+# First check that Leo is installed.
+if ! command -v leo &> /dev/null
+then
+    echo "leo is not installed."
+    exit
+fi
+
+# The private key and address of the debtor.
+# Swap these into program.json, when running transactions as the debtor.
+# "private_key": "APrivateKey1zkpBg8aN5h7rsSUHNEGZetFpUn7dgoutZhorCS1veaiH4YZ"
+# "address": aleo1z5eut5f5x7r45c8ut3xmt5wrsxdkpjrtksn5pu3d9af3zsr8ju8sm64tkz
+
+# The private key and address of the creditor.
+# Swap these into program.json, when running transactions as the creditor.
+# "private_key": "APrivateKey1zkpG9Af9z5Ha4ejVyMCqVFXRKknSm8L1ELEwcc4htk9YhVK"
+# "address": aleo1yzlta2q5h8t0fqe0v6dyh9mtv4aggd53fgzr068jvplqhvqsnvzq7pj2ke
+
+echo "
+###############################################################################
+########                                                               ########
+########             Debtor applies for loan from creditor.            ########
+########                                                               ########
+###############################################################################
+"
+
+# Swap in the private key and address of the debtor to program.json.
 echo "{
   \"program\": \"iou.aleo\",
   \"version\": \"0.0.0\",
@@ -14,21 +41,13 @@ echo "{
     leo run apply || exit
 )
 
-echo "{
-  \"program\": \"iou.aleo\",
-  \"version\": \"0.0.0\",
-  \"description\": \"\",
-  \"development\": {
-      \"private_key\": \"APrivateKey1zkpG9Af9z5Ha4ejVyMCqVFXRKknSm8L1ELEwcc4htk9YhVK\",
-      \"address\": \"aleo1yzlta2q5h8t0fqe0v6dyh9mtv4aggd53fgzr068jvplqhvqsnvzq7pj2ke\"
-  },
-  \"license\": \"MIT\"
-}" > program.json
-
-# Run the `mint_for_demo` function
-(
-    leo run mint_for_demo "aleo1yzlta2q5h8t0fqe0v6dyh9mtv4aggd53fgzr068jvplqhvqsnvzq7pj2ke" "13000u64"
-)
+echo "
+###############################################################################
+########                                                               ########
+########               Creditor disburses loan to debtor.              ########
+########                                                               ########
+###############################################################################
+"
 
 echo "{
   \"program\": \"iou.aleo\",
@@ -46,21 +65,13 @@ echo "{
     leo run disburse || exit
 )
 
-echo "{
-  \"program\": \"iou.aleo\",
-  \"version\": \"0.0.0\",
-  \"description\": \"\",
-  \"development\": {
-      \"private_key\": \"APrivateKey1zkpBg8aN5h7rsSUHNEGZetFpUn7dgoutZhorCS1veaiH4YZ\",
-      \"address\": \"aleo1z5eut5f5x7r45c8ut3xmt5wrsxdkpjrtksn5pu3d9af3zsr8ju8sm64tkz\"
-  },
-  \"license\": \"MIT\"
-}" > program.json
-
-# Run the `mint_for_demo` function
-(
-    leo run mint_for_demo "aleo1z5eut5f5x7r45c8ut3xmt5wrsxdkpjrtksn5pu3d9af3zsr8ju8sm64tkz" "14000u64"
-)
+echo "
+###############################################################################
+########                                                               ########
+########                Debtor repays loan to creditor.                ########
+########                                                               ########
+###############################################################################
+"
 
 echo "{
   \"program\": \"iou.aleo\",
@@ -77,6 +88,14 @@ echo "{
 (
     leo run repay || exit
 )
+
+echo "
+###############################################################################
+########                                                               ########
+########   The creditor confirms the repayment and settles the loan.   ########
+########                                                               ########
+###############################################################################
+"
 
 echo "{
   \"program\": \"iou.aleo\",
